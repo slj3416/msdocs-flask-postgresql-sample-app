d8276b4fc518efc7dc191b468cbd459b0e760bd6
slj3416 <148211999+slj3416@users.noreply.github.com>
2025-05-08 10:28:06 +0000

    hola

--- models.py
+++ models.py
@@ -1,32 +1,15 @@
-from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
-from sqlalchemy.orm import validates
+from flask_sqlalchemy import SQLAlchemy
 
-from app import db
+db = SQLAlchemy()
 
+class ImageData(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(255), nullable=False)
+    red_count = db.Column(db.Integer, nullable=False)
+    green_count = db.Column(db.Integer, nullable=False)
+    blue_count = db.Column(db.Integer, nullable=False)
+    timestamp = db.Column(db.String(50), nullable=False)
+    username = db.Column(db.String(100), nullable=False)
 
-class Restaurant(db.Model):
-    __tablename__ = 'restaurant'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(50))
-    street_address = Column(String(50))
-    description = Column(String(250))
-
-    def __str__(self):
-        return self.name
-
-class Review(db.Model):
-    __tablename__ = 'review'
-    id = Column(Integer, primary_key=True)
-    restaurant = Column(Integer, ForeignKey('restaurant.id', ondelete="CASCADE"))
-    user_name = Column(String(30))
-    rating = Column(Integer)
-    review_text = Column(String(500))
-    review_date = Column(DateTime)
-
-    @validates('rating')
-    def validate_rating(self, key, value):
-        assert value is None or (1 <= value <= 5)
-        return value
-
-    def __str__(self):
-        return f"{self.user_name}: {self.review_date:%x}"
+    def _repr_(self):
+        return f'<ImageData {self.filename}>'
